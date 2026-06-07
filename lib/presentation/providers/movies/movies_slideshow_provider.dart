116d3d26e2d81bf24cb4640f846e7ccc17ec6d17
Jonathan Isaac Quezada Anguiano <jonathan.quezada@itbs-grp.com>
2026-06-07 13:15:54 -0600

    feat: enhance movie presentation with slideshow and horizontal list view, add custom app bar and bottom navigation

---
+++ lib/presentation/providers/movies/movies_slideshow_provider.dart
@@ -0,0 +1,9 @@
+import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:cinemapedia/presentation/providers/movies/movies_providers.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final moviesSlideshowProvider = Provider<List<Movie>>((ref) {
+  final movies = ref.watch( nowPlayingMoviesProvider );
+  if ( movies.isEmpty ) return [];
+  return movies.sublist(0, 6);
+});
--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:cinemapedia/presentation/providers/movies/movies_providers.dart';
+import 'package:cinemapedia/presentation/providers/movies/movies_slideshow_provider.dart';
+import 'package:cinemapedia/presentation/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -9,8 +11,8 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Home")),
-      body: _HomeView()
+      body: _HomeView(),
+      bottomNavigationBar: const CustomBottomNavigation(),
     );
   }
 }
@@ -19,7 +21,6 @@ class _HomeView extends ConsumerStatefulWidget {
   const _HomeView();
 
   @override
-  
   _HomeViewState createState() => _HomeViewState();
 }
 
@@ -27,26 +28,43 @@ class _HomeViewState extends ConsumerState<_HomeView> {
   @override
   initState() {
     super.initState();
-    ref.read( nowPlayingMoviesProvider.notifier ).loadNextPage();
+    ref.read(nowPlayingMoviesProvider.notifier).loadNextPage();
   }
+
   @override
   Widget build(BuildContext context) {
-    final nowPlayingMovies = ref.watch( nowPlayingMoviesProvider );
-    if ( nowPlayingMovies.isEmpty || nowPlayingMovies.length < 20 ) {
-      return const Center(
-        child: CircularProgressIndicator(),
-      );
+    final slideShowMovies = ref.watch(moviesSlideshowProvider);
+    final nowPlayingMovies = ref.watch(nowPlayingMoviesProvider);
+    if (slideShowMovies.isEmpty) {
+      return const Center(child: CircularProgressIndicator());
     }
-    return ListView.builder(
-      itemCount: nowPlayingMovies.length,
-      itemBuilder: (context, index) {
-        final movie = nowPlayingMovies[index];
-        return ListTile(
-          title: Text(movie.title),
-          subtitle: Text(movie.overview),
-          leading: Image.network(movie.posterPath) ,
-        );
-      },
+    return Column(
+      children: [
+        CustomAppBar(),
+        MoviesSlideshow(movies: slideShowMovies),
+        MovieHorizontalListview(
+          movies: nowPlayingMovies,
+          title: 'En cines',
+          subTitle: 'Lunes 20',
+          loadNextPage: () {
+            ref.read(nowPlayingMoviesProvider.notifier).loadNextPage();
+          },
+        ),
+
+        // Expanded(
+        //   child: ListView.builder(
+        //     itemCount: slideShowMovies.length,
+        //     itemBuilder: (context, index) {
+        //       final movie = slideShowMovies[index];
+        //       return ListTile(
+        //         title: Text(movie.title),
+        //         subtitle: Text(movie.overview),
+        //         leading: Image.network(movie.posterPath) ,
+        //       );
+        //     },
+        //   ),
+        // ),
+      ],
     );
   }
 }
